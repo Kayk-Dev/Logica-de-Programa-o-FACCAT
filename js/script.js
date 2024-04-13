b8ff36fdbf15fcb872403742c3ae4a3d9b56ae6e
kazinho-ux <kakamgk00@gmail.com>
2024-04-13 19:06:08 -0300

    atuallização de functions

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ document.querySelector('.hamburger-menu').addEventListener('click', function() {
     document.querySelector('.menu-list').classList.toggle('active');
   });
 
+function antecessor(){
+    let numero1 = parseInt(prompt("Digite um numero para saber seu antecessor: "))
+    let ante = numero1 - 1
+    alert("O antecessor do seu numero é " + ante) 
+}
