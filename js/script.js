82c86086a3dbe3bef8d02d23cde1b5528e3df2d0
kazinho-ux <kakamgk00@gmail.com>
2024-04-13 19:19:59 -0300

    atuallização de functions

--- js/script.js
+++ js/script.js
@@ -5,5 +5,19 @@ document.querySelector('.hamburger-menu').addEventListener('click', function() {
 function antecessor(){
     let numero1 = parseInt(prompt("Digite um numero para saber seu antecessor: "))
     let ante = numero1 - 1
-    alert("O antecessor do seu numero é " + ante) 
+    alert("O antecessor do seu numero é " + ante)
+}
+
+function sucessor(){
+    let numero1 = parseInt(prompt("Digite um numero para saber seu sucessor: "))
+    let suce = numero1 + 1
+    alert("O sucessor do seu numero é " + suce)
+}
+
+function areaRetangulo(){
+    let base = parseInt(prompt("Digite a base do seu retangulo: "))
+    let altura = parseInt(prompt("Agora a altura do seu retangulo: "))
+
+    let area = base * altura
+    alert("A area do seu retangulo é: " + area)
 }
